27503427e7c1b9f3cd82f8a0885330e412c0419b
kiran04-code <kr551344@gmail.com>
2025-06-24 00:52:37 +0530

    bakend work start

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-const express = require("express")
 
+import  express from "express"
+import  connectionwithDB  from "./config/db.js"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
-
-const port = 6002
+connectionwithDB(process.env.MONGODB_URL).then(()=>{
+  console.log("mongodb is Connected")
+}).catch((err)=>{
+    console.log("ERROR",err)
+})
+const port =  process.env.PORT  || 6002
 app.get("/api/status",(req,res)=>{
     res.send("Server is Working! with 200  Status Code")
 })
+
 app.listen(port,(req,res)=>{
     console.log(`Server is running on port http://localhost:${port}`)
 })
