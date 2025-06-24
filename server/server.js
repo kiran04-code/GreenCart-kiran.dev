caab836f5bf9608aebaefd01f2515ab138b3ca08
kiran04-code <kr551344@gmail.com>
2025-06-24 08:41:48 +0530

    init-6

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@
 import  express from "express"
 import  connectionwithDB  from "./config/db.js"
 import dotenv from "dotenv"
+import routes from "./routes/user.js"
+import cors from "cors"
 dotenv.config()
 const app = express()
+const allOrigies = ["http://localhost:5173/"]
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({
+    origin:allOrigies,
+    credentials:true
+}))
 connectionwithDB(process.env.MONGODB_URL).then(()=>{
   console.log("mongodb is Connected")
 }).catch((err)=>{
@@ -13,7 +22,7 @@ const port =  process.env.PORT  || 6002
 app.get("/api/status",(req,res)=>{
     res.send("Server is Working! with 200  Status Code")
 })
-
+app.use("/api",routes)
 app.listen(port,(req,res)=>{
     console.log(`Server is running on port http://localhost:${port}`)
 })
